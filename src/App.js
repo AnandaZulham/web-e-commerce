558ed43a1bad0586713473073526e7d785d590bf
Ananda Zulham <anandazulham@gmail.com>
2021-02-03 17:42:54 +0700

    Add Font Roboto and Add Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Header from './Header';
+import 'fontsource-roboto';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
